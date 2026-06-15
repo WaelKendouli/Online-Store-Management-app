418395c86d01b1b868dad2cfbe79514d88ed4cd5
WaelKendouli <kendouliwael@gmail.com>
2026-06-15 18:08:14 +0100

    Addding clsNumberGenerator class to generate track numbers randomly

---
+++ UI_Tools/clsNumberGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStoreProject.UI_Tools
+{
+    public class clsNumberGenerator
+    {
+        private static Random _random = new Random();
+        public static string GenerateRandomTrackingNumber()
+        {
+            int length = _random.Next(12, 21);
+            return GenerateAlphanumeric(length);
+        }
+
+        // Helper: Generate alphanumeric string (A-Z, 0-9)
+        private static string GenerateAlphanumeric(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+                sb.Append(chars[_random.Next(chars.Length)]);
+
+            return sb.ToString();
+        }
+
+        // Helper: Generate numeric string only
+        private static string GenerateNumeric(int length)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+                sb.Append(_random.Next(0, 10));
+
+            return sb.ToString();
+        }
+    }
+}

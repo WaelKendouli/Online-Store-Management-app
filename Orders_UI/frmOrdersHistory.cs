6f03e72a0fc173eb3c60171a6e02c8baceb6b5c5
WaelKendouli <kendouliwael@gmail.com>
2026-06-24 17:31:58 +0100

    Hnadeling loading data in the history form

--- Orders_UI/frmOrdersHistory.cs
+++ Orders_UI/frmOrdersHistory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LogicLayer;
+using OnlineStoreProject.UI_Tools;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,24 @@ namespace OnlineStoreProject.Orders_UI
 {
     public partial class frmOrdersHistory : Form
     {
-        public frmOrdersHistory()
+        int _CustomerID = 0;
+        DataTable _dtOrdersHistory = new DataTable();
+        public frmOrdersHistory(int CustomerID)
         {
             InitializeComponent();
+            _CustomerID = CustomerID;
+            
+        }
+        public async void LoadData()
+        {
+            dgvOrdersHistory.DataSource = _dtOrdersHistory;
+            _dtOrdersHistory = await clsOrder.GetHistoryOfOrdersPerCustomerAsync(_CustomerID);
+            clsSunnyUIDataGridViewSettings.LoadData(_dtOrdersHistory, dgvOrdersHistory,
+                cbItems, new List<string> { "Amount" , "Quantity" , "OrderTime" }, 170);
+        }
+        private void frmOrdersHistory_Load(object sender, EventArgs e)
+        {
+            LoadData();
         }
     }
 }
